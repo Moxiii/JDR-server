808c63373c8d2bc342aeb3eafdd84910a4be22e2
maxime lapouge <dpower632@gmail.com>
2024-11-23 13:22:50 +0100

    Correction of WebMvcConfig

--- src/main/java/com/moxi/jdrserver/Config/WebMvcConfig.java
+++ src/main/java/com/moxi/jdrserver/Config/WebMvcConfig.java
@@ -5,11 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:19006/") // pour bien etre sur d'autoriser le front a faire des requete (pas obligatoire)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
+    };
 }
