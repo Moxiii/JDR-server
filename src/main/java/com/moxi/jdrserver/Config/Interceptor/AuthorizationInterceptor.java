7a694c41a34b0f3168f62262b9fcf7d8e2f1b2f5
maxime lapouge <dpower632@gmail.com>
2025-01-19 15:49:30 +0100

    added filter and utils

--- src/main/java/com/moxi/jdrserver/Config/Interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/moxi/jdrserver/Config/Interceptor/AuthorizationInterceptor.java
@@ -1,25 +1,33 @@
 package com.moxi.jdrserver.Config.Interceptor;
 
 import com.moxi.jdrserver.Config.Annotation.RequireAuthorization;
+import com.moxi.jdrserver.Config.Utils.JwtUtils;
+import com.moxi.jdrserver.Config.Utils.SecurityUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
+  @Autowired
+  private JwtUtils jwtUtils;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String authorization = request.getHeader("Authorization");
         HandlerMethod handlerMethod = (HandlerMethod) handler;
         RequireAuthorization requireAuthorization = handlerMethod.getMethodAnnotation(RequireAuthorization.class);
-        //TODO implement JWT for real auth :)
-        //PS postman request now require Authorization header with True value
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/api/auth")) {
+            return true;
+        }
         if (requireAuthorization != null) {
-            if (authorization != null && authorization.startsWith("True ")) {
-                return true;
+           if(!SecurityUtils.isAuthenticated(request , jwtUtils)){
+               response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+               return false;
             }
         }
-        return false;
+        return true;
     }
 }
--- src/main/java/com/moxi/jdrserver/Config/SecurityConfig.java
+++ src/main/java/com/moxi/jdrserver/Config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.moxi.jdrserver.Config;
 
+import com.moxi.jdrserver.Config.Filter.JwtAuthFilter;
+import com.moxi.jdrserver.Config.Utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtUtils jwtUtils;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -17,6 +24,9 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .requestMatchers("/api/**").permitAll()
+                .and()
+                .addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 .and()
                 .formLogin()
                 .loginPage("/api/auth/login") // Specify the URL of your custom login page
