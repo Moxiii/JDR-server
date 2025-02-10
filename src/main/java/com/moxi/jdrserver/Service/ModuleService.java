bd70bcc20b4e2021676786c88b78bd0d76974573
timdecool <tim.decool@gmail.com>
2025-02-10 18:08:06 +0100

    Module CRUD

--- src/main/java/com/moxi/jdrserver/Service/ModuleService.java
+++ src/main/java/com/moxi/jdrserver/Service/ModuleService.java
@@ -1,13 +1,15 @@
 package com.moxi.jdrserver.Service;
 
+import com.moxi.jdrserver.DTO.ModuleDTO;
 import com.moxi.jdrserver.Models.User;
 import com.moxi.jdrserver.Models.Module;
 import com.moxi.jdrserver.Repository.ModuleRepository;
-import com.moxi.jdrserver.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ModuleService {
@@ -17,9 +19,28 @@ public class ModuleService {
     public List<Module> findAll() {
         return moduleRepository.findAll();
     }
+
+    public Optional<Module> findById(int id) {
+        return moduleRepository.findById(id);
+    }
     public List<Module> findByUser(User user) {
         return moduleRepository.findByCreatedBy(user);
     }
 
+    public Module create(Module module) {
+        return moduleRepository.save(module);
+    }
 
+    public Module update(int id, ModuleDTO moduleDTO) {
+        Module module = this.findById(id).orElseThrow();
+        module.setTitle(moduleDTO.getTitle());
+        module.setDescription(moduleDTO.getDescription());
+        module.setType(moduleDTO.getType());
+        module.setUpdatedAt(LocalDateTime.now());
+        return moduleRepository.save(module);
+    }
+
+    public void delete(int id) {
+        moduleRepository.deleteById(id);
+    }
 }
